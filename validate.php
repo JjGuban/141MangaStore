9c3201d5099f2b2261e3f2956018d61ecafaed97
JjGuban <167877370+JjGuban@users.noreply.github.com>
2024-11-02 03:28:41 +0800

    validate.php

--- validate.php
+++ validate.php
@@ -2,7 +2,27 @@
 // core/validate.php
 
 function validatePassword($password) {
-    return (strlen($password) >= 8);
+    if (strlen($password) >= 8) {
+        $hasLower = false;
+        $hasUpper = false;
+        $hasNumber = false;
+
+        for ($i = 0; $i < strlen($password); $i++) {
+            if (ctype_lower($password[$i])) {
+                $hasLower = true;
+            } elseif (ctype_upper($password[$i])) {
+                $hasUpper = true;  
+            } elseif (ctype_digit($password[$i])) {
+                $hasNumber = true;
+            }
+
+            if ($hasLower && $hasUpper && $hasNumber) {
+                return true;
+            }
+        }
+    }
+    
+    return false;
 }
 
 function sanitizeInput($input) {
